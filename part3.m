e09d6507f89eba7aefb18e27f767527d48aea049
sujitv19196 <sujit5817@gmail.com>
2023-10-24 19:04:02 -0500

    change

--- part3.m
+++ part3.m
@@ -5,12 +5,12 @@ load('circular.mat');
 R = 0.208;  % Radius of the antenna array in meters
 f = 5.5e9;  % Frequency of the signal in Hz
 
-% Calculate the time difference between consecutive timestamps
-time_diff = diff(t);
-angular_velocity = 360 / (12.25 / time_diff(1));  % Degrees per second
-
-% Calculate angular positions (φ) for each measurement in radians
-phi = cumsum(angular_velocity * time_diff) * pi / 180; % Convert to radians
+% % Calculate the time difference between consecutive timestamps
+% time_diff = diff(t);
+% angular_velocity = 360 / (12.25 / time_diff(1));  % Degrees per second
+% 
+% % Calculate angular positions (φ) for each measurement in radians
+% phi = cumsum(angular_velocity * time_diff) * pi / 180; % Convert to radians
 
 % Create an array to store the multipath profile
 theta_values = -180:180;  % Adjusted θ' values at 3-degree intervals
@@ -28,7 +28,7 @@ for i = 1:length(theta_values)
 end
 
 % Normalize the multipath profile for better visualization
-multipath_profile = multipath_profile / max(multipath_profile);
+% multipath_profile = multipath_profile / max(multipath_profile);
 
 % Load the provided multipath profile values
 load('MultipathProfile.mat');
@@ -36,7 +36,8 @@ load('MultipathProfile.mat');
 % Plot both the calculated multipath profile and the provided values
 plot(theta_values, multipath_profile, 'b', 'LineWidth', 2);
 hold on;
-plot(theta_values, P, 'r', 'LineWidth', 2);
+theta_values3 = -180:3:180;
+plot(theta_values3, P, 'r', 'LineWidth', 2);
 hold off;
 
 xlabel('θ'' (degrees)');
