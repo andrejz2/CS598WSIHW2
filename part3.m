94ba549c70fa67bf747941809169f98d413ad820
sujitv19196 <sujit5817@gmail.com>
2023-10-29 17:03:14 -0500

    finish part 3

--- part3.m
+++ part3.m
@@ -4,12 +4,14 @@ load('circular.mat');
 % Constants
 R = 0.208;  % Radius of the antenna array in meters
 f = 5.5e9;  % Frequency of the signal in Hz
+speed_of_light = 299792458; % meters per second
 
-% Calculate the time difference between consecutive timestamps
-time_diff = diff(t);
-angular_velocity = 360 / (12.25 / time_diff(1));  % Degrees per second
-% Calculate angular positions (φ) for each measurement in radians
-phi = cumsum(angular_velocity * time_diff) * pi / 180; % Convert to radians
+% Define rotations per ms and initial angular position
+rotations_per_ms = 1 / 12.25 / 1000;  % Example value
+initial_angular_position = 0;  % Initial angle in degrees (adjust as needed)
+
+% Calculate angular position for each time value
+phi = initial_angular_position + (rotations_per_ms * 360 * t);
 
 % Create an array to store the multipath profile
 theta_values = -180:180;  % Adjusted θ' values at 3-degree intervals
@@ -19,15 +21,12 @@ multipath_profile = zeros(size(theta_values));
 for i = 1:length(theta_values)
     theta_prime = theta_values(i);
     mpsum = zeros(size(t));
-    for j = 1:length(h)-1
+    for j = 1:length(h)
         hk = h(j,2) / h(j,1);
-        mpsum(j) = abs((hk * exp(1j * 2 * pi * f * R * cos(phi(j)-theta_prime)))).^2;
+        mpsum(j) = hk * exp(-1j * 2 * pi * (f/speed_of_light) * R * cosd(phi(j)-theta_prime));
     end
-    multipath_profile(i) = sum(mpsum); 
+    multipath_profile(i) = abs(sum(mpsum)).^2; 
 end
-
-% Normalize the multipath profile for better visualization
-% multipath_profile = multipath_profile / max(multipath_profile);
 
 % Load the provided multipath profile values
 load('MultipathProfile.mat');
@@ -40,7 +39,7 @@ hold on;
 hold off;
 
 xlabel('θ'' (degrees)');
-ylabel('Normalized Power (P)');
+ylabel('Power (P)');
 title('Multipath Profile Comparison');
 legend('Calculated', 'Provided');
 grid on;
